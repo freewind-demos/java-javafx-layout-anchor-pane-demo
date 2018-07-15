6acde7eb2a505cf8b95bb0f70ebc9ee5e9000a36
freewind <nowindlee@gmail.com>
2018-07-15 15:13:47 +0800

    anchor pane demo

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -2,8 +2,8 @@ package demo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
@@ -14,9 +14,15 @@ public class Hello extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
-        Pane root = new Pane() {{
-            getChildren().add(new Label("Hello, JavaFX!"));
+
+        AnchorPane root = new AnchorPane() {{
+            Button button1 = new Button("Hello1");
+            Button button2 = new Button("Hello2");
+            getChildren().addAll(button1, button2);
+            AnchorPane.setTopAnchor(button1, 20.0 /* offset to top */);
+            AnchorPane.setTopAnchor(button2, 40.0 /* offset to top */);
         }};
+
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
